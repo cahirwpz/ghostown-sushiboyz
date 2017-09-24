63aafd9b669273f6dd9096418385608f7825e6f7
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-09-24 15:11:32 +0200

    Fix known problems with worker thread.

--- a500/sushiboyz/worker.c
+++ a500/sushiboyz/worker.c
@@ -5,36 +5,48 @@
 #include <proto/exec.h>
 
 #include "common.h"
+#include "memory.h"
 #include "worker.h"
 #include "tasks.h"
 
 typedef struct Task TaskT;
 typedef struct List ListT;
-typedef struct Node NodeT;
-typedef struct MinNode MinNodeT;
 
-#define LIST() &(struct List){}
+#define STACKSIZE 4096 /* because Inflate takes ~2.8 KiB of stack */
 
-static struct Task *workerTask = NULL;
+static TaskT workerTask = {
+  {
+    .ln_Type = NT_TASK,
+    .ln_Pri = -10,
+    .ln_Name = "Worker Task"
+  }
+};
+
 static volatile BOOL workerDrain = FALSE;
-static ListT *workQueue = LIST();
-static ListT *workQueueNotEmpty = LIST();
-static ListT *workDone = LIST();
+static ListT workQueue;
+static ListT workQueueNotEmpty;
+static ListT workDone;
 
 static void WorkerTask() {
-  Log("[Init] Worker task %lx started!\n", (LONG)FindTask(NULL));
+  TaskT *this = FindTask(NULL);
+
+  Log("[Init] Worker task %lx started!\n", (LONG)this);
+
+  /* Become a background task. */
+  SetTaskPri(this, -10);
 
   for (;;) {
     /* The non-empty condition must be rechecked,
      * as the task may get preempted before RemHead executes. */
-    TaskWait(workQueueNotEmpty);
+    while (IsListEmpty(&workQueue))
+      TaskWait(&workQueueNotEmpty);
 
     for (;;) {
       WorkT *work;
 
       /* Dequeue an action to run. */
       Forbid();
-      work = (WorkT *)RemHead(workQueue);
+      work = (WorkT *)RemHead(&workQueue);
       if (work)
         work->state = WS_RUNNING;
       Permit();
@@ -49,7 +61,7 @@ static void WorkerTask() {
       /* Signal all tasks that were waiting for an action to become DONE. */
       Forbid();
       work->state = WS_DONE;
-      TaskSignal(workDone);
+      TaskSignal(&workDone);
       Permit();
     }
   }
@@ -58,8 +70,11 @@ static void WorkerTask() {
 void WorkAdd(WorkT *work) {
   Forbid();
   if (!workerDrain) {
+    BOOL wasEmpty = IsListEmpty(&workQueue);
     work->state = WS_READY;
-    AddTail(workQueue, (NodeT *)work);
+    AddTail(&workQueue, (NodeT *)&work->link);
+    if (wasEmpty)
+      TaskSignal(&workQueueNotEmpty);
   }
   Permit();
 }
@@ -67,16 +82,26 @@ void WorkAdd(WorkT *work) {
 /* XXX Assumes that current task has higher priority than worker! */
 void WorkWaitFor(WorkT *work) {
   while (work->state == WS_READY || work->state == WS_RUNNING)
-    TaskWait(workDone);
+    TaskWait(&workDone);
 }
 
 void WorkerStart() {
-  /* TODO Should not use system memory allocator! */
-  workerTask = CreateTask("Worker Task", -10, WorkerTask, 1024);
+  NewList(&workQueue);
+  NewList(&workQueueNotEmpty);
+  NewList(&workDone);
 
-  NewList(workQueue);
-  NewList(workQueueNotEmpty);
-  NewList(workDone);
+  {
+    APTR stack = MemAlloc(STACKSIZE, MEMF_PUBLIC|MEMF_CLEAR);
+
+    workerTask.tc_SPLower = stack;
+    workerTask.tc_SPUpper = stack + STACKSIZE;
+    workerTask.tc_SPReg = stack + STACKSIZE;
+
+    AddTask(&workerTask, WorkerTask, NULL);
+  }
+
+  /* Make sure worker task had an opportunity to run. */
+  TaskYield();
 }
 
 void WorkerShutdown() {
@@ -87,24 +112,25 @@ void WorkerShutdown() {
   Forbid();
   {
     WorkT *work;
-    while ((work = (WorkT *)RemHead(workQueue)))
+    while ((work = (WorkT *)RemHead(&workQueue)))
       work->state = WS_ABORTED;
     /* If someone was waiting for a work to finish, we've just aborted it. */
-    TaskSignal(workDone);
+    TaskSignal(&workDone);
   }
   Permit();
 
   {
     TaskT *self = FindTask(NULL);
     /* Lower our priority to let worker task to finish. */
-    BYTE pri = SetTaskPri(self, workerTask->tc_Node.ln_Pri);
+    BYTE pri = SetTaskPri(self, workerTask.tc_Node.ln_Pri);
     /* Wait for the worker to suspend on workQueueNotEmpty */
-    while (workerTask->tc_State != TS_WAIT)
+    while (workerTask.tc_State != TS_WAIT)
       TaskYield();
     /* Restore our priority. */
     SetTaskPri(self, pri);
   }
 
   /* Now worker task is safe to remove. */
-  RemTask(workerTask);
+  RemTask(&workerTask);
+  MemFree(workerTask.tc_SPLower);
 }
--- a500/sushiboyz/worker.h
+++ a500/sushiboyz/worker.h
@@ -3,14 +3,19 @@
 
 #include <exec/nodes.h>
 
+typedef struct Node NodeT;
+typedef struct MinNode MinNodeT;
+
 typedef enum __attribute__((packed)) {
   WS_NEW, WS_READY, WS_RUNNING, WS_ABORTED, WS_DONE
 } WorkStateT;
 
+typedef void (*WorkFuncT)(void * asm("a0"));
+
 typedef struct {
-  struct MinNode link;
+  MinNodeT link;
   volatile WorkStateT state;
-  void (*func)(void *);
+  WorkFuncT func;
   void *data;
 } WorkT;
 
